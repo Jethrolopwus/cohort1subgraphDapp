07c76ed7fe5c3d99904d4434da031ea48fbc93e5
Emmanuel Doji <devopsnormad@gmail.com>
2024-08-30 17:51:10 +0100

    new changes on Hero, footer and navbar

--- Components/SharedComponents/Navbar.tsx
+++ Components/SharedComponents/Navbar.tsx
@@ -1,23 +1,22 @@
 import Image from "next/image";
 import Link from "next/link";
-import logo from "@/public/assets/logo1.jpeg";
+import BLogo from "@/public/assets/Logo2.png";
 
 const Navbar = () => {
   return (
     <div>
-      <div className="flex border-2 bg-red-700 justify-between items-center">
-      <div>
-        Token
+      <div className="flex bg-gradient-to-r from-primary-50 via-primary-200 to-primary-300 justify-between items-center relative z-10">
+      <Link href="/">
+        <div className="flex items-center cursor-pointer">
+          <Image src={BLogo} alt="Blockfuse Labs Logo" className="w-24" />
+          <h1 className="font-bold text-white text-xl ml-0">BLOCKFUSE LABS</h1>
+        </div>
+      </Link>
+        <div className="mr-8">
+          <input type="text" placeholder="Search" className="rounded-md px-2 py-2 w-60" />
+        </div>
       </div>
-      <div className=" mr-8">
-        <input type="text" placeholder="Search" className="" />
-      </div>
-      </div>
-       
     </div>
-  
-      
-      
   );
 };
 
